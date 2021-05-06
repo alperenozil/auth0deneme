54fa67985da9c272046fee0f76d11bfad6dc71e4
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-05-06 22:26:57 +0300

    add login to project

--- auth0deneme/ContentView.swift
+++ auth0deneme/ContentView.swift
@@ -6,11 +6,30 @@
 //
 
 import SwiftUI
+import Auth0
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+        
+        Button("Login") {
+            Auth0
+                .webAuth()
+                .scope("openid profile")
+                .audience("https://alperen.us.auth0.com/userinfo")
+                .start { result in
+                    switch result {
+                    case .failure(let error):
+                        // Handle the error
+                        print("Error: \(error)")
+                    case .success(let credentials):
+                        // Do something with credentials e.g.: save them.
+                        // Auth0 will automatically dismiss the login page
+                        print("Credentials: \(credentials)")
+                    }
+            }
+        }
     }
 }
 
