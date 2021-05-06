0c44260790ff97744e64b86d212bfa14aec2ab86
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-05-06 22:05:56 +0300

    Initial Commit

---
+++ auth0deneme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  auth0deneme
+//
+//  Created by Necdet Alperen Özil on 6.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
